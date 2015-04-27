006e7f3be4ab072f59dbb2a529dccd8df1755816
David Richardson <drichardson@enigmait.ca>
2015-04-27 11:16:05 -0500

    Testing SpotPrices controller.

--- public_html/src/Controller/SpotpricesController.php
+++ public_html/src/Controller/SpotpricesController.php
@@ -109,4 +109,20 @@ class SpotpricesController extends AppController
         }
         return $this->redirect(['action' => 'index']);
     }
+    
+    public function update() 
+    {
+        $okcoin_client = new OKCoin(
+            new OKCoin_ApiKeyAuthentication($ok_api_key, $ok_secret_key)
+        );
+
+        $okc_btc_ticker = $okcoin_client->tickerApi(array('symbol' => 'btc_usd'));
+        $okc_ltc_ticker = $okcoin_client->tickerApi(array('symbol' => 'ltc_usd'));
+
+        echo "<pre>" . print_r($okc_btc_ticker, TRUE) . "</pre>";
+        echo "<pre>" . print_r($okc_ltc_ticker, TRUE) . "</pre>";
+
+        echo "<pre>" . print_r($this->Spotprices->read(null, 1), TRUE) . "</pre>";
+        die('REACHED update():: die');
+    }
 }
