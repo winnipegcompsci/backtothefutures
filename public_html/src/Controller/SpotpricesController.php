bff62b412e4c967a1d99145b6973c8534b41dda3
David Richardson <drichardson@enigmait.ca>
2015-04-28 12:33:46 -0500

    Checking $this->OKCoin().

--- public_html/src/Controller/SpotpricesController.php
+++ public_html/src/Controller/SpotpricesController.php
@@ -119,16 +119,17 @@ class SpotpricesController extends AppController
         $ok_secret_key = Configure::read('okcoin.secretkey');
         
         
-        $okcoin_client = new OKCoin(
+        $okcoin_client = new $this->OKCoin(
             new OKCoin_ApiKeyAuthentication($ok_api_key, $ok_secret_key)
         );
                         
-        $okc_btc_ticker = $okcoin_client->tickerApi(array('symbol' => 'btc_usd'));
-        $okc_ltc_ticker = $okcoin_client->tickerApi(array('symbol' => 'ltc_usd'));
+        $okc_btc_ticker = $okcoin_client->tickerFutureApi(array('symbol' => 'btc_usd'));
+        $okc_ltc_ticker = $okcoin_client->tickerFutureApi(array('symbol' => 'ltc_usd'));
 
         echo "<pre>" . print_r($okc_btc_ticker, TRUE) . "</pre>";
         echo "<pre>" . print_r($okc_ltc_ticker, TRUE) . "</pre>";
         
         die('END OF PROCESSING');
     }
+    
 }
