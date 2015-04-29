1bcdd4489b7b0a15d2f7617d24b83bc4c5551738
David Richardson <drichardson@enigmait.ca>
2015-04-29 12:56:16 -0500

    Testing timestamp.

--- public_html/src/Shell/OKCoinShell.php
+++ public_html/src/Shell/OKCoinShell.php
@@ -27,12 +27,36 @@ class OKCoinShell extends Shell
         $btc_ticker = $client->tickerFutureApi(array('symbol' => 'btc_usd', 'contract_type' => 'this_week'));
         $ltc_ticker = $client->tickerFutureApi(array('symbol' => 'ltc_usd', 'contract_type' => 'this_week'));
         
-        foreach($this->Currencies->find('all') as $currency) {
-            $this->out('Currency: ' . print_r($currency, TRUE) );
+        foreach($this->Sources->find('all') as $source) {
+            if($source->source_name == "OKCoin") {
+                $okcoin = $source;
+            } 
         }
         
+        foreach($this->Currencies->find('all') as $currency) {
+            if($currency->currency_name == "bitcoin") {
+                $bitcoin = $currency; 
+            } else if($currency->currency_name == "litecoin") {
+                $litecoin = $currency;
+            }
+        }           
         
-        
+        $spotprice = $this->SpotPrices->newEntity();      
+        $spotprice->currency_id = $bitcoin->id;
+        $spotprice->source_id = $okcoin->id;
+        $spotprice->low = $btc_ticker->ticker->low;
+        $spotprice->high = $btc_ticker->ticker->high;
+        $spotprice->last = $btc_ticker->ticker->last;
+        $spotprice->buy = $btc_ticker->ticker->buy;
+        $spotprice->sell = $btc_ticker->ticker->sell;
+        $spotprice->volume = $btc_ticker->ticker->vol;
+        $spotprice->timestamp = date('Y-m-d H:i:s', $btc_ticker->date);
+                
+        if($this->SpotPrices->save($spotprice)) {
+            $this->out('Saved New Spotprice');
+        } else {
+            $this->out('Failed to save Spotprice');
+        }
     }
     
     
