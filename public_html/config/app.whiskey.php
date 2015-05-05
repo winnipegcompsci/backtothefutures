0f61ca023f3dc87a7c9f09289c5ba103b01b9c47
David Richardson <drichardson@enigmait.ca>
2015-05-05 13:24:27 -0500

    Trying new config.

--- public_html/config/app.whiskey.php
+++ public_html/config/app.whiskey.php
@@ -220,7 +220,7 @@ return [
              */
             //'port' => 'nonstandard_port_number',
             'username' => 'root',
-            'password' => '1qaz2wsx',
+            'password' => '',
             'database' => 'backtothefutures',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
